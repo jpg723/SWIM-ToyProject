ce8928545dc9a33b89de9a82563441af73bf3d57
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2023-07-14 19:40:09 +0900

    [UPDATE] 새글쓰기

--- demo/src/main/frontend/src/Components/Study/WriteForm.js
+++ demo/src/main/frontend/src/Components/Study/WriteForm.js
@@ -1,3 +1,5 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import "./WriteForm.css";
 
 function WriteForm() {
@@ -47,8 +49,8 @@ function WriteForm() {
         <div id="WriteForm-1txt">스터디 기본정보를 입력 해주세요.</div>
         <div id="WriteForm-1txt-info">
           <div id="WriteForm-1txt-info-select">
-            <div  id="WriteForm-option"><text id="s">카테고리</text></div>
-            <select>
+            <text id="WriteForm-option">카테고리</text>
+            <select id="WriteForm-option_value">
               <option value="language">어학</option>
               <option value="employment">취업</option>
               <option value="gosi">고시/공무원</option>
@@ -57,35 +59,47 @@ function WriteForm() {
               <option value="etc">가타</option>
             </select>
           </div>
+
           <div id="WriteForm-1txt-info-select">
-            <text>모집인원</text>
-            <select>
+            <text id="WriteForm-option">모집인원</text>
+            <select id="WriteForm-option_value">
               <option value="direct">직접 입력</option>
               <option value="Undefined">인원 미정</option>
             </select>
           </div>
+
           <div id="WriteForm-1txt-info-select">
-            <text>진행방식</text>
-            <select>
+            <text id="WriteForm-option">진행방식</text>
+            <select id="WriteForm-option_value">
               <option value="on">온라인</option>
               <option value="off">오프라인</option>
             </select>
           </div>
+
           <div id="WriteForm-1txt-info-select">
-            <text>지역</text>
-            <select>option_repeat()</select>
+            <text id="WriteForm-option">지역</text>
+            <select id="WriteForm-option_value">option_repeat()</select>
           </div>
         </div>
       </div>
       <div className="WriteForm-2">
         <div id="WriteForm-1txt">스터디를 소개해주세요</div>
         <div id="WriteForm-2-info">
-          <div>제목</div>
-          <input></input>
-          {/* 글쓰는 공간 추가 */}
+          <div id="WriteForm-2-info-title_txt">제목</div>
           <div>
-            <button>취소</button>
-            <button>등록</button>
+            <input
+              type="text"
+              placeholder="제목을 입력해주세요"
+              id="WriteForm-2-info-title_input"
+            />
+          </div>
+
+          <div id="quill">
+            <ReactQuill />
+          </div>
+          <div id="WriteForm-2-info-btns">
+            <button id="WriteForm-2-info-btns-cancle">취소</button>
+            <button id="WriteForm-2-info-btns-save">등록</button>
           </div>
         </div>
       </div>
