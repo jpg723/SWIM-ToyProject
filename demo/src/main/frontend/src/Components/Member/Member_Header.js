9ec5f72a5eba912480a2e978c2616f03c1ec8ae7
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2023-09-03 00:07:54 +0900

    [UPDATE] login logo/ 설명추가

--- demo/src/main/frontend/src/Components/Member/Member_Header.js
+++ demo/src/main/frontend/src/Components/Member/Member_Header.js
@@ -1,11 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
 import "./Member_Header.css";
+import logo from "../../img/swimming.png";
 
 function Member_Header() {
   return (
     <div>
       <Link to="/" id="Member-header">
-        <div id="Member-logo-image"></div>
+        <img src={logo} id="Member-logo-image"></img>
         <text id="Member-logo-text">SWIM</text>
       </Link>
         <Outlet />
