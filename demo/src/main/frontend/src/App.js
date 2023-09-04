2cca1194af5b12c56d45b4fca20d75b75c476571
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-09-04 16:06:50 +0900

    [update] 로그인 상태 관리 구현

--- demo/src/main/frontend/src/App.js
+++ demo/src/main/frontend/src/App.js
@@ -13,27 +13,30 @@ import Member_Header from "./Components/Member/Member_Header";
 import Search_ID from "./Components/Member/Search_ID";
 import Search_password from "./Components/Member/Search_password";
 import Signup from "./Components/Member/Signup";
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        {/* Header,Footer를 보여주고 싶은 컴포넌트 */}
-        <Route element={<Header />}>
-          <Route path="/" element={<Main />} />
-          <Route path="/write-form" element={<WriteForm />} />
-          <Route path="/study-list" element={<StudyList />} />
-          <Route path="/study-view" element={<StudyView />} />
-        </Route>
-        {/* Header,Footer를 안 보여주고 싶은 컴포넌트 */}
-        <Route element={<Member_Header />}>
-          <Route path="/sign-up" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/search_id" element={<Search_ID />} />
-          <Route path="/search_pw" element={<Search_password />} />
-        </Route>
-      </Routes>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <Routes>
+          {/* Header,Footer를 보여주고 싶은 컴포넌트 */}
+          <Route element={<Header />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/write-form" element={<WriteForm />} />
+            <Route path="/study-list" element={<StudyList />} />
+            <Route path="/study-view" element={<StudyView />} />
+          </Route>
+          {/* Header,Footer를 안 보여주고 싶은 컴포넌트 */}
+          <Route element={<Member_Header />}>
+            <Route path="/sign-up" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/search_id" element={<Search_ID />} />
+            <Route path="/search_pw" element={<Search_password />} />
+          </Route>
+        </Routes>
+      </div>
+    </RecoilRoot>
   );
 }
 
