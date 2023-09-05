67646f6d1d6ba466063b7842180e132853c1e113
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-09-05 13:21:27 +0900

    [update] 프로필 변경

--- demo/src/main/frontend/src/Components/Main/Header.js
+++ demo/src/main/frontend/src/Components/Main/Header.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "../../img/swimming.png";
+import profile from "../../img/profile.png";
 import { useEffect } from 'react';
 import { useRecoilState } from "recoil";
 import { loginAtom } from "../../atoms";
@@ -34,8 +35,9 @@ function Header() {
             <button id="new-wirte-btn">새 글쓰기</button>
           </Link>
 
-          {login ? (<div></div>):
-          (<Link to="/login" id="header-botton-box2">
+          {login ? 
+          (<div id="header_profile"><img src={profile} id="image-profile"></img></div>)
+          :(<Link to="/login" id="header-botton-box2">
             <button id="login-btn">Log in</button>
           </Link>)
           }
