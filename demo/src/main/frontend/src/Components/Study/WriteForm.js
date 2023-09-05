eef06f13fda741d72ae3959b2ae82260b45bc663
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-09-05 20:59:25 +0900

    [UPDATE] 새글쓰기 CSS 수정

--- demo/src/main/frontend/src/Components/Study/WriteForm.js
+++ demo/src/main/frontend/src/Components/Study/WriteForm.js
@@ -2,8 +2,22 @@ import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./WriteForm.css";
+import { useNavigate } from 'react-router-dom';
 
 function WriteForm() {
+
+  const [study_num, setStudy_num] = useState("undefined");
+
+  const handleSelect = (e) => {
+    setStudy_num(e.target.value);
+  };
+
+  /*취소 버튼 클릭시 */
+  const navigate = useNavigate();
+  function write_cancle(){
+    navigate(`/`);
+  }
+
   // 지역 value도 한글로 전달
   let [region_option, set_region_option] = useState([
     "전체",
@@ -36,18 +50,24 @@ function WriteForm() {
               <option value="gosi">고시/공무원</option>
               <option value="hobby">취미/교양</option>
               <option value="program">프로그래밍</option>
-              <option value="etc">가타</option>
+              <option value="etc">타</option>
             </select>
           </div>
 
           <div id="WriteForm-1txt-info-select">
             <text id="WriteForm-option">모집인원</text>
-            <select id="WriteForm-option_value">
-              <option value="direct">직접 입력</option>
+            <select id="WriteForm-option_value" onChange={handleSelect} value={study_num}>
               <option value="Undefined">인원 미정</option>
+              <option value="direct">직접 입력</option>
             </select>
           </div>
-
+          {study_num === "direct" ?
+          (<div id="WriteForm-1txt-info-select">
+            <input type="text" id="selboxDirect" name="selboxDirect"/>
+          </div>):(<div></div>)
+          }
+        </div>
+        <div id="WriteForm-1txt-info">
           <div id="WriteForm-1txt-info-select">
             <text id="WriteForm-option">진행방식</text>
             <select id="WriteForm-option_value">
@@ -71,9 +91,8 @@ function WriteForm() {
         </div>
       </div>
       <div className="WriteForm-2">
-        <div id="WriteForm-1txt">스터디를 소개해주세요</div>
+        <div id="WriteForm-1txt">스터디를 소개해주세요.</div>
         <div id="WriteForm-2-info">
-          <div id="WriteForm-2-info-title_txt">제목</div>
           <div>
             <input
               type="text"
@@ -86,7 +105,7 @@ function WriteForm() {
             <ReactQuill />
           </div>
           <div id="WriteForm-2-info-btns">
-            <button id="WriteForm-2-info-btns-cancle">취소</button>
+            <button id="WriteForm-2-info-btns-cancle" onClick={write_cancle}>취소</button>
             <button id="WriteForm-2-info-btns-save">등록</button>
           </div>
         </div>
